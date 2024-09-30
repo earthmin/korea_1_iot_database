b14522deda423bb0f3ef1c60fb2be6c4e54efb13
earthmin <fltk4286@naver.com>
2024-10-01 07:07:38 +0900

    20241001

---
+++ j_정규화/제2정규화.sql
@@ -0,0 +1,11 @@
+use employee;
+create table employee (
+	employee_id int,
+    employee_name varchar(50),
+    location varchar(50),
+    supervisior_id int,
+    department varchar(50)
+);
+
+
+    
